d8e3fb050a9cf64a01e64d3dcf0cfea42003d238
creynolds183 <creynolds183@ivytech.edu>
2026-01-22 15:18:21 +0000

    Finished M2A2

---
+++ TriangleClass.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class TriangleClass {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a side length");
+        Double side1 = input.nextDouble();
+        System.out.println("Enter a side length");
+        Double side2 = input.nextDouble();
+        System.out.println("Enter a side length");
+        Double side3 = input.nextDouble();
+        System.out.println("Enter a color");
+        String color = input.next();
+        System.out.println("Is the triangle filled true/false");
+        Boolean filled = input.nextBoolean();
+
+        Triangle triangle1 = new Triangle(side1, side2, side3);
+        triangle1.setColor(color);
+        triangle1.setFilled(filled);
+
+        System.out.println("triangle1 area: " + triangle1.getArea() + " triangle1 perimeter: " + triangle1.getPerimeter() + " triangle1 color: " + triangle1.getColor() + " triangle1 filled: " + triangle1.getFilled());
+
+    }
+}
+
+
+class Triangle extends GeometricObject{
+    private Double side1 = 1.0;
+    private Double side2 = 1.0;
+    private Double side3 = 1.0;
+
+    public Triangle(){
+
+    }
+    
+    public Triangle(Double side1, Double side2, Double side3){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public Double getSide1(){
+        return side1;
+    }
+
+    public void setSide1(Double side1){
+        this.side1 = side1;
+    }
+
+    public Double getSide2(){
+        return side2;
+    }
+
+    public void setSide2(Double side2){
+        this.side2 = side2;
+    }
+
+    public Double getSide3(){
+        return side3;
+    }
+
+    public void setSide3(Double side3){
+        this.side3 = side3;
+    }
+
+    public Double getArea(){
+        Double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s* ((s - side1) * (s - side2) * ( s -side3)));
+    }
+
+    public Double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    public String toString() {
+        return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+    }
+}
+
+class GeometricObject{
+    private String color = "white";
+    private Boolean filled;
+    private java.util.Date dateCreated;
+
+
+    public GeometricObject(){
+        dateCreated = new java.util.Date();
+    }
+
+    public GeometricObject(String color, boolean filled){
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setFilled(Boolean filled){
+         this.filled = filled;
+    }
+
+    public java.util.Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public String toString() {
+        return "created on " + dateCreated + "\ncolor: " + color + "and filled: " + filled;
+    }
+}
